0938eee7c0a978e1ae3ec5e85bcc179eae3594f2
H. Littles <hlittles@eVantageGroup.com>
2018-09-22 15:03:53 -0500

    upto but not including 'Testing the  Response Context Objects Directly'

--- toDo/functional_tests/tests.py
+++ toDo/functional_tests/tests.py
@@ -116,7 +116,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertnotIn('Buy peaock feathers', page_text)
+        self.assertNotIn('Buy peaock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
