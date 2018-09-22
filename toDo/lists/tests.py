bf567c0bfdeea6f63a6ba9a8bc90a94a87d3af9a
H. Littles <hlittles@eVantageGroup.com>
2018-09-21 22:44:29 -0500

    updates

--- toDo/lists/tests.py
+++ toDo/lists/tests.py
@@ -1,19 +1,34 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 #from lists.views import home_page
 
 # Create your tests here:.
 
-class ItemModelTest(TestCase):
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+class ListAndModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -21,8 +36,10 @@ class ItemModelTest(TestCase):
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.ssertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -41,15 +58,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class HomePageTest(TestCase):
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
 
 class NewListTest(TestCase):
 
