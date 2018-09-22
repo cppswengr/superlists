6ee84664757074cd268aecbf1db11b37120f66bd
Brandon <Brandon@eVantageGroup.com>
2018-09-22 10:50:11 -0500

    updated models to suppor lists:

--- toDo/lists/models.py
+++ toDo/lists/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class  Item(models.Model):
-    text = models.TextField(default='')
-
 class List(models.Model):
     text = models.TextField(default='')
+
+class  Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+    # the List is formed by filtering by Item
+    # e.g., bobs_list = Item.objects.filter(related_list=list)
+
